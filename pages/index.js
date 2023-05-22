8209ca0e6ed4bc8335ede042ae134c6d4eb28680
zhucebuliaomax <tooclose@live.cn>
2023-05-22 21:36:06 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -45,18 +45,13 @@ export default function Home({ posts }) {
 
         <footer className={styles.footer}>
           <div>
-            <a href="https://twitter.com/ShrimpMr" target="_blank">
-              Twitter
+            <hr></hr>
+            <a>
+              <abbr title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License." style="cursor: help;">
+                CC BY-NC 4.0
+              </abbr> 
+                2023 © Max.
             </a>
-               · 
-            <a href="https://github.com/zhucebuliaomax" target="_blank">
-              GitHub
-            </a>
-               · 
-            <a href="https://instagram.com/unoldtree" target="_blank">
-              Instagram
-            </a>
-            <small><abbr title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License." style="cursor: help;">CC BY-NC 4.0</abbr> 2023 © Max.</small>
           </div>
         </footer>
       </main>
